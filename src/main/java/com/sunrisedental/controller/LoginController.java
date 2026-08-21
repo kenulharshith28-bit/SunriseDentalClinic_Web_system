979770114d9e19803b3ad7c279ef9f4636a9fe03
Harshith <kenulharshith28@gmail.com>
2026-08-21 22:29:21 +0530

    test pass implement login authentication

--- src/main/java/com/sunrisedental/controller/LoginController.java
+++ src/main/java/com/sunrisedental/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.sunrisedental.controller;
 
 import com.sunrisedental.service.AuthenticationService;
 
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +26,21 @@ public class LoginController extends HttpServlet {
             final HttpServletResponse response)
             throws ServletException, IOException {
 
-        // Login handling will be added after the test fails.
+        final String username =
+                request.getParameter("username");
+
+        final String password =
+                request.getParameter("password");
+
+        try {
+            authenticationService.authenticate(
+                    username,
+                    password);
+
+        } catch (SQLException exception) {
+            throw new ServletException(
+                    "Unable to authenticate user",
+                    exception);
+        }
     }
 }
